a2b9f184fe332c6e95bbb9d4e929069eccec25c0
Jeremy Nelson <jnelson@overkill.epicsol.org>
2016-01-30 10:32:30 -0600

    New EPIC!

--- source/irc.c
+++ source/irc.c
@@ -40,8 +40,8 @@
 /*
  * irc_version is what $J returns, its the common-name for the version.
  */
-const char irc_version[] = "EPIC4-2.10.5";
-const char useful_info[] = "epic4 2 10 5";
+const char irc_version[] = "EPIC4-2.10.6";
+const char useful_info[] = "epic4 2 10 6";
 
 /*
  * internal_version is what $V returns, its the integer-id for the
@@ -52,7 +52,7 @@ const char internal_version[] = "20080904";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 782;
+const unsigned long	commit_id = 783;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
